76355d129ac39637b7cfbb910762bafd10b8e831
Tathagat Sathe <66299313+tathagatsathe@users.noreply.github.com>
2025-03-23 17:30:46 +0530

    Time: 19 ms (87.15%) | Memory: 23.2 MB (79.45%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -1,26 +1,31 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        ans = [0]*len(nums)
+        n = len(nums)
+        ans = []
         mul = 1
-        zeroExist= 0
+        zeroCount= 0
         idx = 0
         for i, val in enumerate(nums):
             if(val==0):
-                zeroExist+=1
-                if(zeroExist == 1):
-                    idx = i
+                zeroCount+=1
+                idx = i
+                if(zeroCount>1):
+                    return [0]*n
             else:    
                 mul*=val
 
-        if zeroExist>1:
-            return ans
-        
-        if zeroExist == 1:
-            ans[idx] = mul
-            return ans
+        # if zeroCount == 1:
+        #     ans[idx] = mul
+        #     return ans
 
         for i, val in enumerate(nums):
-            ans[i] = int(mul/val)
+            if(zeroCount>0):
+                if(val!=0):
+                    ans.append(0)
+                else:
+                    ans.append(mul)
+            else:
+                ans.append(mul//val)
 
         return ans
 
