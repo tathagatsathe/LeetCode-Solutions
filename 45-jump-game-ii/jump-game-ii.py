7e5d094d453156b42d590283a521bdedbbec87a2
Tathagat Sathe <66299313+tathagatsathe@users.noreply.github.com>
2025-03-23 15:14:01 +0530

    Time: 4 ms (89.90%) | Memory: 18.6 MB (74.59%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.py
+++ 45-jump-game-ii/jump-game-ii.py
@@ -3,11 +3,8 @@ class Solution:
         ans = 0
         k = 0
         mx_idx = 1
-        if(len(nums)==1):
-            return ans
 
         while(mx_idx<len(nums)):
-            # print(nums[k], mx_idx, ans, k)
             j = nums[k]
             ans+=1
             if(k+j+1>=len(nums)):
@@ -17,11 +14,8 @@ class Solution:
                 if(nums[i]+i>=mx):
                     mx=nums[i]+i
                     mx_idx = i
-                # if(nums[i]>=nums[mx_idx]):
-                #     mx_idx = i
             k = mx_idx
             mx_idx = k+1
-            # ans+=1
 
         return ans
         
