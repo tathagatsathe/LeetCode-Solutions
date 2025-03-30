2abce699c94eec0884fad8dce4df2272426321fa
Tathagat Sathe <66299313+tathagatsathe@users.noreply.github.com>
2025-03-30 12:33:28 +0530

    Time: 15 ms (70.37%) | Memory: 19.9 MB (70.27%) - LeetSync

--- 135-candy/candy.py
+++ 135-candy/candy.py
@@ -1,40 +1,13 @@
 class Solution:
     def candy(self, ratings: List[int]) -> int:
         n = len(ratings)
-        ans = 0
-        prev = 0
-        i=0
-        j = 1
-        count = 0
+        c = [1]*n
+        for i in range(1,n):
+            if(ratings[i]>ratings[i-1]):
+                c[i] = c[i-1]+1
+        
+        for i in range(n-2,-1,-1):
+            if(ratings[i]>ratings[i+1]):
+                c[i] = max(c[i],c[i+1]+1)    
 
-        if(n==1):
-            return 1
-        while(j<n):
-            if(j<n and ratings[j]==ratings[j-1]):
-                count=1
-                while(j<n and ratings[j]==ratings[j-1]):
-                    count+=1
-                    j+=1
-                
-                ans+=count - min(prev, 1)
-                prev = 1
-
-            count=1
-            if(j<n and ratings[j]<ratings[j-1]):
-                while(j<n and ratings[j]<ratings[j-1]):
-                    count+=1
-                    j+=1
-                
-                ans+=((count+1)*count)//2 - min(prev,count)
-                prev = 1
-            # print(count, j, prev, ans)
-            count=1
-            if(j<n and ratings[j]>ratings[j-1]):
-                while(j<n and ratings[j]>ratings[j-1]):
-                    count+=1
-                    j+=1
-                ans+=((count+1)*count)//2 - min(prev,count)
-                prev = count
-
-
-        return ans
+        return sum(c)
