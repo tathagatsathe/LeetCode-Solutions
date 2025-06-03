15b826d73cb3f0c66d20d53e05fc7d724076eac1
Tathagat Sathe <66299313+tathagatsathe@users.noreply.github.com>
2025-06-03 11:20:47 +0530

    Time: 1 ms (8.06%) | Memory: 17.7 MB (92.08%) - LeetSync

--- 388-longest-absolute-file-path/longest-absolute-file-path.py
+++ 388-longest-absolute-file-path/longest-absolute-file-path.py
@@ -1,7 +1,5 @@
 class Solution:
     def lengthLongestPath(self, input: str) -> int:
-        # input.replace('\n','\n/')
-        # print('input: ',input)
         arr = input.split('\n')
         temp = []
         st = []
@@ -11,11 +9,9 @@ class Solution:
             sr = i.replace('\t','')
             temp.append({'t':t_c, 's': sr})
 
-        # print('temp: ',temp)
-        # return 20
+
         st.append({'c': 0, 't': -1})
         for i in temp:
-            # print(st)
             while(st[-1]['t']>=i['t']):
                 st.pop()
             ln = st[-1]['c']+len(i['s'])
@@ -23,16 +19,6 @@ class Solution:
                 ln+=1
             if("." in i['s'] and ln>mx):
                 mx = ln
-            # print(ln)
             st.append({'c': ln, 't': i['t']})
 
-        # [{'t': 0, 's': 'dir'}, {'t': 1, 's': 'subdir1'}, {'t': 1, 's': 'subdir2'}, {'t': 2, 's': 'file.ext'}]
-
-        # print('st: ',st)
-        
         return mx
-
-        # ['dir', 'subdir1', 'file1.ext', 'subsubdir1', 'subdir2', 'subsubdir2']
-        # ['dir', '\tsubdir1', '\t\tfile1.ext', '\t\tsubsubdir1', '\tsubdir2', '\t\tsubsubdir2', '\t\t\tfile2.ext']
-        # 3 -> 10 -> 20
-        # 0 ->  
