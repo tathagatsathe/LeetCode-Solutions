dc9ac9faa09ae497fba2621cb92b2e05320da132
Tathagat Sathe <66299313+tathagatsathe@users.noreply.github.com>
2025-06-04 16:32:55 +0530

    Time: 49 ms (39.33%) | Memory: 18.4 MB (90.42%) - LeetSync

--- 1049-minimum-domino-rotations-for-equal-row/minimum-domino-rotations-for-equal-row.py
+++ 1049-minimum-domino-rotations-for-equal-row/minimum-domino-rotations-for-equal-row.py
@@ -1,29 +1,20 @@
 class Solution:
     def minDominoRotations(self, tops: List[int], bottoms: List[int]) -> int:
-        t = {}
-        b = {}
         i = 0
-        s = set()
         l = len(tops)
         p = -1
         c=0
-        while(i < l):
-            if tops[i] not in t:
-                t[tops[i]]=0
-            
-            if bottoms[i] not in b:
-                b[bottoms[i]] = 0
+        t = [0]*(7)
+        b = [0]*(7)
 
+        while(i < l):
             if(tops[i]==bottoms[i]):
                 p = tops[i]
                 c+=1
-
             t[tops[i]]+=1
             b[bottoms[i]]+=1
-            s.add(tops[i])
-            s.add(bottoms[i])
             i+=1
-        # print('p: ',p, c)
+        
         if(p!=-1):
             if(t[p]+b[p]==l+c):
                 return min(t[p]-c,b[p]-c)
@@ -31,17 +22,9 @@ class Solution:
                 return -1
 
         ans = l+1
-        # print('s: ',s)
-        # print('t: ',t)
-        # print('b: ',b)
-        for k in s:
-            if(k not in t):
-                t[k]=0
-            if(k not in b):
-                b[k]=0
+        for k in range(1,7):
             if(t[k]+b[k]==l):
                 mn = min(t[k],b[k])
-                print('mn: ',mn,'k: ',k)
                 if(mn<ans):
                     ans = mn
 
