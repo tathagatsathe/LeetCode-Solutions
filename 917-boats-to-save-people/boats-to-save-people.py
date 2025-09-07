170e8caf9fefc09540f132401d530205309b366f
Tathagat Sathe <66299313+tathagatsathe@users.noreply.github.com>
2025-09-08 01:12:59 +0530

    Time: 51 ms (32.96%) | Memory: 23.2 MB (57.22%) - LeetSync

--- 917-boats-to-save-people/boats-to-save-people.py
+++ 917-boats-to-save-people/boats-to-save-people.py
@@ -4,12 +4,10 @@ class Solution:
         ans=0
         i=0
         j=len(people)-1
-        while(i<j):
+        while(i<=j):
             if(people[i]+people[j]<=limit):
                 i+=1
             j-=1
-            ans+=1
-        if(i==j):
             ans+=1
 
         return ans
