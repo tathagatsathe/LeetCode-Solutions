c94606954d5e1b118ffd47b7260eab5a0eaee96d
Tathagat Sathe <66299313+tathagatsathe@users.noreply.github.com>
2025-10-02 22:02:56 +0530

    Time: 16 ms (53.79%) | Memory: 23 MB (40.00%) - LeetSync

--- 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.py
+++ 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.py
@@ -6,42 +6,24 @@
 #         self.right = right
 class Solution:
     def maxPathSum(self, root: Optional[TreeNode]) -> int:
-        ans = root.val
+        ans = float("-inf")
 
         def maxPath(node):
             nonlocal ans
 
-            if node.left==None and node.right==None:
-                if node.val > ans:
-                    ans = node.val
-                return [node.val]
+            if node == None:
+                return 0
 
-            temp = node.val
-            q1 = node.val
-            q2 = node.val
-            if node.left != None:
-                p1 = maxPath(node.left)
-                p1_max = max(p1)
-                temp+=p1_max
-                q1+=p1_max
-                # print(node.val, p1)
-                # print(node.val, p1, temp, q1, q2)
-            if node.right != None:
-                p2 = maxPath(node.right)
-                p2_max = max(p2)
-                temp+=p2_max
-                q2+=p2_max
-                # print(node.val, p2)
+            l = max(maxPath(node.left),0)
+            r = max(maxPath(node.right),0)
+            ans = max(node.val+l+r, ans)
 
-            temp = max(max(temp, max(q1,q2)),node.val)
-
-            if temp > ans:
-                ans = temp
-            # print('arr: ',[q1, q2, node.val])
-            return [q1, q2, node.val]
-
+            return node.val +max(l,r)
+            
         maxPath(root)
 
         return ans
+
+
 
         
