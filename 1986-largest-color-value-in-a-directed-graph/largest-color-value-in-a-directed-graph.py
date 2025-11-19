86bd0f05d4889ff68097fd9ae551307114e8f9f5
Tathagat Sathe <66299313+tathagatsathe@users.noreply.github.com>
2025-11-20 00:03:55 +0530

    Time: 1347 ms (99.59%) | Memory: 61.8 MB (99.80%) - LeetSync

--- 1986-largest-color-value-in-a-directed-graph/largest-color-value-in-a-directed-graph.py
+++ 1986-largest-color-value-in-a-directed-graph/largest-color-value-in-a-directed-graph.py
@@ -9,8 +9,6 @@ class Solution:
                 color_dict[color] = idx
                 idx+=1
 
-        # print(color_dict)
-
         n = len(colors)
         dp = [[0]*no_of_colors for _ in range(n)]
         adj = [[] for _ in range(n)]
@@ -21,27 +19,22 @@ class Solution:
             adj[v[0]].append(v[1])
             indegree[v[1]]+=1
 
-        # print('indegree: ',indegree)
         queue = deque()
         for i in range(n):
             if indegree[i] == 0:
                 queue.append(i)
                 dp[i][color_dict[colors[i]]]+=1
 
-        # print('dp: ',dp)
         if len(queue) == 0:
             return -1
 
-        # print('queue: ',queue)
         while queue:
             for _ in range(len(queue)):
                 node = queue.popleft()
                 visited[node] = 2
-                # print('inside loop: ',adj[node], node)
                 for ad in adj[node]:
                     if visited[ad] == 2:
                         return -1
-                    # if visited[ad] != 1:
                     for j in range(no_of_colors):
                         if j == color_dict[colors[ad]]:
                             if dp[ad][j]>dp[node][j]:
@@ -56,12 +49,6 @@ class Solution:
                     if indegree[ad] == 0:
                         queue.append(ad)
 
-            # print(queue,'__________')
-            # print('DP: ',node, dp)
-        
-        # print('indegree: ',indegree)
-        # print('dp: ',dp)
-        # print('adj: ',adj)
         if sum(indegree)>0:
             return -1
         mx = 0
@@ -75,25 +62,3 @@ class Solution:
 
 
         return mx
-
-# [1, 0, 0, 0] 0
-# [2, 0, 0, 0] 1
-# [1, 1, 0, 0] 2
-# [2, 1, 0, 0] 3
-# [2, 1, 1, 0] 4
-# [2, 2, 0, 0] 5
-# [3, 2, 0, 0] 6
-# [3, 3, 0, 0] 7
-# [3, 3, 0, 3] 8
-# [3, 3, 0, 4] 9
-
-# [1, 0, 0, 0] 0
-# [2, 0, 0, 0] 1
-# [1, 1, 0, 0] 2
-# [2, 1, 0, 0] 3
-# [2, 1, 1, 0] 4
-# [2, 2, 0, 0] 5
-# [0, 0, 0, 0] 6
-# [1, 2, 0, 0] 7
-# [2, 1, 0, 1] 8
-# [2, 1, 0, 1] 9
