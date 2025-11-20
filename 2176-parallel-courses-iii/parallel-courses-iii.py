5c3ec8f7e6c0fdd0252feb8885fbf895838d02fd
Tathagat Sathe <66299313+tathagatsathe@users.noreply.github.com>
2025-11-20 12:37:39 +0530

    Time: 127 ms (84.78%) | Memory: 40 MB (91.46%) - LeetSync

---
+++ 2176-parallel-courses-iii/parallel-courses-iii.py
@@ -0,0 +1,41 @@
+from collections import deque
+class Solution:
+    def minimumTime(self, n: int, relations: List[List[int]], time: List[int]) -> int:
+        adj = [[] for _ in range(n)]
+        indegree = [0]*n
+        dp = [float("inf")]*n
+        visited = [0]*n
+        for rel in relations:
+            adj[rel[0]-1].append(rel[1]-1)
+            indegree[rel[1]-1]+=1
+
+        # print('adj: ',adj)
+        # print('indegree: ',indegree)
+        queue = deque()
+
+        for i in range(n):
+            if indegree[i] == 0:
+                queue.append(i)
+                dp[i] = time[i]
+
+        # print('queue: ',queue)
+        # print('dp: ',dp)
+
+        while queue:
+            for _ in range(len(queue)):
+                node = queue.popleft()
+                visited[node] = 2
+                for ad in adj[node]:
+                    if visited[ad] == 0:
+                        dp[ad] = time[ad] + time[node]
+                    dp[ad] = max(dp[ad], dp[node] + time[ad])
+                    indegree[ad]-=1
+                    if indegree[ad] == 0:
+                        queue.append(ad)
+                    visited[ad] = 1
+
+            # print('dpp: ',dp)
+
+        # print(dp)
+
+        return max(dp)
