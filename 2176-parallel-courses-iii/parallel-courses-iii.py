aa441c82432d21809fd6c0192d5a149581eb5b09
Tathagat Sathe <66299313+tathagatsathe@users.noreply.github.com>
2025-11-20 12:40:39 +0530

    Time: 107 ms (97.38%) | Memory: 39.6 MB (92.49%) - LeetSync

--- 2176-parallel-courses-iii/parallel-courses-iii.py
+++ 2176-parallel-courses-iii/parallel-courses-iii.py
@@ -3,14 +3,11 @@ class Solution:
     def minimumTime(self, n: int, relations: List[List[int]], time: List[int]) -> int:
         adj = [[] for _ in range(n)]
         indegree = [0]*n
-        dp = [float("inf")]*n
-        visited = [0]*n
+        dp = [0]*n
+        queue = deque()
         for rel in relations:
             adj[rel[0]-1].append(rel[1]-1)
             indegree[rel[1]-1]+=1
-
-
-        queue = deque()
 
         for i in range(n):
             if indegree[i] == 0:
@@ -20,14 +17,11 @@ class Solution:
         while queue:
             for _ in range(len(queue)):
                 node = queue.popleft()
-                visited[node] = 2
                 for ad in adj[node]:
-                    if visited[ad] == 0:
-                        dp[ad] = time[ad] + time[node]
-                    dp[ad] = max(dp[ad], dp[node] + time[ad])
+                    if dp[ad] < dp[node]+ time[ad]:
+                        dp[ad] = dp[node] + time[ad]
                     indegree[ad]-=1
                     if indegree[ad] == 0:
                         queue.append(ad)
-                    visited[ad] = 1
 
         return max(dp)
