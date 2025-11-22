ff4a19bd3895f903280d7cadf3320932126ea09e
Tathagat Sathe <66299313+tathagatsathe@users.noreply.github.com>
2025-11-22 22:16:55 +0530

    Time: 117 ms (41.10%) | Memory: 28.3 MB (69.54%) - LeetSync

---
+++ 1325-path-with-maximum-probability/path-with-maximum-probability.py
@@ -0,0 +1,33 @@
+import heapq
+
+class Solution:
+    def maxProbability(self, n: int, edges: List[List[int]], succProb: List[float], start_node: int, end_node: int) -> float:
+        visited = [False]*n
+        grid = [[] for _ in range(n)]
+
+        for i in range(len(edges)):
+            grid[edges[i][0]].append((edges[i][1], succProb[i]))
+            grid[edges[i][1]].append((edges[i][0], succProb[i]))
+
+        h = []
+
+        heapq.heappush(h, (-1, start_node))
+        ans = 0
+        while h:
+            max_prob, node = heapq.heappop(h)
+            # print(h)
+            # print('node: ', node)
+            # print('max_prob: ',max_prob)
+            if node == end_node:
+                if max_prob*-1>ans:
+                    ans = -1 * max_prob
+            if visited[node] == True:
+                continue
+            visited[node] = True
+            for g in grid[node]:
+                if visited[g[0]] == False:
+                    heapq.heappush(h, (max_prob*g[1], g[0]))
+
+        # print(grid)
+
+        return ans
