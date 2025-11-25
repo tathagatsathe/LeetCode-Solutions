1971b6919f0190fea377e0a836af1d828ef224e3
Tathagat Sathe <66299313+tathagatsathe@users.noreply.github.com>
2025-11-25 22:52:45 +0530

    Time: 1640 ms (16.00%) | Memory: 35.8 MB (64.50%) - LeetSync

---
+++ 2711-minimum-time-to-visit-a-cell-in-a-grid/minimum-time-to-visit-a-cell-in-a-grid.py
@@ -0,0 +1,51 @@
+from collections import deque
+import heapq
+class Solution:
+    def minimumTime(self, grid: List[List[int]]) -> int:
+
+        def getValidVertices(i, j, time):
+            vertices = []
+            directions = [(-1,0),(0,-1),(1,0),(0,1)]
+
+            for x, y in directions:
+                if 0<=x+i<m and 0<=y+j<n and grid[x+i][y+j]<=time+1:
+                    vertices.append((x+i, y+j))
+
+            return vertices
+        
+        m = len(grid)
+        n = len(grid[0])
+        directions = [(-1,0),(0,-1),(1,0),(0,1)]
+        visited = [[False]*n for _ in range(m)]
+        queue = deque()
+        h = []
+        heapq.heappush(h,(0,0,0))
+        # queue.append((0,0,0))
+
+        vertices = getValidVertices(0,0,0)
+        if vertices == [] or grid[0][0]!=0:
+            return -1
+
+        while h:
+            # time, i, j = queue.popleft()
+            time, i, j = heapq.heappop(h)
+
+            if visited[i][j]:
+                continue
+
+            grid[i][j] = time
+            if i == m-1 and j==n-1:
+                return grid[i][j]
+            visited[i][j] = True
+            for x, y in directions:
+                if 0<=x+i<m and 0<=y+j<n:
+                    if grid[x+i][y+j]<=time+1:
+                        # queue.append((time+1,x+i,y+j))
+                        heapq.heappush(h, (time+1,x+i,y+j))
+                    else:
+                        temp = 0 if (grid[x+i][y+j]-grid[i][j])%2  else 1
+                        # queue.append((grid[x+i][y+j]+temp, x+i,y+j))
+                        heapq.heappush(h, (grid[x+i][y+j]+temp, x+i,y+j))
+
+
+        return -1
