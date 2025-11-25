12b37c7dd5f3ba159249fab8ae1152e20d16dd52
Tathagat Sathe <66299313+tathagatsathe@users.noreply.github.com>
2025-11-25 23:01:27 +0530

    Time: 739 ms (89.00%) | Memory: 31.1 MB (93.50%) - LeetSync

--- 2711-minimum-time-to-visit-a-cell-in-a-grid/minimum-time-to-visit-a-cell-in-a-grid.py
+++ 2711-minimum-time-to-visit-a-cell-in-a-grid/minimum-time-to-visit-a-cell-in-a-grid.py
@@ -8,22 +8,21 @@ class Solution:
         visited = [[False]*n for _ in range(m)]
         h = []
         heapq.heappush(h,(0,0,0))
-
+        visited[0][0] = True
         if grid[0][0]!=0 or (grid[0][1]>1 and grid[1][0]>1):
             return -1
 
         while h:
             time, i, j = heapq.heappop(h)
 
-            if visited[i][j]:
-                continue
-
             grid[i][j] = time
+
             if i == m-1 and j==n-1:
                 return grid[i][j]
             visited[i][j] = True
             for x, y in directions:
                 if 0<=x+i<m and 0<=y+j<n and visited[x+i][y+j]==False:
+                    visited[x+i][y+j] = True
                     if grid[x+i][y+j]<=time+1:
                         heapq.heappush(h, (time+1,x+i,y+j))
                     else:
