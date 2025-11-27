5fa66412b278c00c6dd31bc9ace28d458ae21c82
Tathagat Sathe <66299313+tathagatsathe@users.noreply.github.com>
2025-11-27 21:51:05 +0530

    Time: 21 ms (30.03%) | Memory: 20.8 MB (95.38%) - LeetSync

--- 1039-find-the-town-judge/find-the-town-judge.py
+++ 1039-find-the-town-judge/find-the-town-judge.py
@@ -1,13 +1,13 @@
 class Solution:
     def findJudge(self, n: int, trust: List[List[int]]) -> int:
-        indegrees = [0]*(n+1)
+        indegrees = [[0]*2 for _ in range(n+1)]
         adj = [[] for _ in range(n+1)]
         for a, b in trust:
-            indegrees[b]+=1
-            adj[a].append(b)
+            indegrees[b][0]+=1
+            indegrees[a][1]+=1
 
         for i in range(1,n+1):
-            if indegrees[i] == n-1 and adj[i]==[]:
+            if indegrees[i][0] == n-1 and indegrees[i][1]==0:
                 return i
 
         return -1
