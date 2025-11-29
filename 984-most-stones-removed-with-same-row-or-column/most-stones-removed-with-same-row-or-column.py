08a79fadf5433a18df83024d1b2c2cec01e3ee8d
Tathagat Sathe <66299313+tathagatsathe@users.noreply.github.com>
2025-11-29 17:32:50 +0530

    Time: 706 ms (10.63%) | Memory: 18.3 MB (87.30%) - LeetSync

--- 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.py
+++ 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.py
@@ -1,27 +1,28 @@
 class Solution:
     def removeStones(self, stones: List[List[int]]) -> int:
         n = len(stones)
-        graph = [[] for _ in range(n)]
+        parent = list(range(n))
+
+        def find(a):
+            if parent[a]!=a:
+                return find(parent[a])
+            return parent[a]
+
+        def union(a, b):
+            rootA = find(a)
+            rootB = find(b)
+
+            if rootA!=rootB:
+                parent[rootB] = rootA
 
         for i in range(n):
             for j in range(i+1, n):
                 if i!=j and (stones[i][0]==stones[j][0] or stones[i][1]==stones[j][1]):
-                    graph[i].append(j)
-                    graph[j].append(i)
+                    union(i, j)
 
-        visited = [False]*n
-        def dfs(node):
-            if visited[node]:
-                return False
-            visited[node] = True
-            for nei in graph[node]:
-                dfs(nei)
-
-            return True
-
-        ans = n
+        p = set()
         for i in range(n):
-            if dfs(i):
-                ans-=1
+            p.add(find(i))
+        ans = n - len(p)
 
         return ans
