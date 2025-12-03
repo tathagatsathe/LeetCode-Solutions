ae5120eddfe97678593bda7c0b46bf5233be4088
Tathagat Sathe <66299313+tathagatsathe@users.noreply.github.com>
2025-12-03 12:45:24 +0530

    Time: 1379 ms (32.12%) | Memory: 55.2 MB (22.86%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.py
@@ -1,31 +1,30 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
-        nums.sort()
         s = sum(nums)
-        if(s%2==1):
+        n = len(nums)
+        if s%2!=0:
             return False
 
-        s = int(s/2)
-        dp = [[None]*(s+1) for _ in range(len(nums)+1)]
-        i = 0
-        
-        def fn(nums, s, i):
-            if(dp[i][s]!=None):
-                return dp[i][s]
-            if(s==0):
-                dp[i][s] = True
-                return True
-            if(len(nums)==i or s<0):
-                dp[i][s] = False
+        s = s//2
+        dp = [[None]*(s+1) for _ in range(n+1)]
+        def fn(i, s):
+            if i>=n:
                 return False
-            s1 = fn(nums, s-nums[i], i+1)
-            s2 = fn(nums, s, i+1)
-            if(s1==True or s2==True):
+
+            if s == 0:
+                return True
+            if dp[i][s]!=None:
+                return dp[i][s]
+            temp = fn(i+1, s-nums[i])
+            if temp:
+                dp[i][s] = temp
+                return True
+            temp2 = fn(i+1, s)
+            if temp2:
                 dp[i][s] = True
                 return True
             dp[i][s] = False
             return False
 
-        return fn(nums, s, 0)
+        return fn(0, s)
 
-        
