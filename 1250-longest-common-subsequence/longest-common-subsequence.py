e7404f87ff2082c06114289799af104c9fbf00d2
Tathagat Sathe <66299313+tathagatsathe@users.noreply.github.com>
2025-12-04 09:13:51 +0530

    Time: 490 ms (49.57%) | Memory: 42.9 MB (46.85%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.py
+++ 1250-longest-common-subsequence/longest-common-subsequence.py
@@ -1,13 +1,18 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        dp = [[0]*(len(text2)+1) for _ in range(len(text1)+1)]
+        m = len(text1)
+        n = len(text2)
+        dp = [[0]*n for _ in range(m)]
 
-        for i in range(1,len(text1)+1):
-            for j in range(1, len(text2)+1):
-                if(text1[i-1]==text2[j-1]):
-                    dp[i][j] = dp[i-1][j-1] + 1
+        for i in range(m):
+            for j in range(n):
+                if i == 0 and j == 0:
+                    dp[i][j] = 1 if text1[i]==text2[j] else 0
+                elif i == 0:
+                    dp[i][j] = 1 if text1[i]==text2[j] else dp[i][j-1]
+                elif j == 0:
+                    dp[i][j] = 1 if text1[i]==text2[j] else dp[i-1][j]
                 else:
-                    dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+                    dp[i][j] = dp[i-1][j-1] + 1 if text1[i]==text2[j] else max(dp[i][j-1],dp[i-1][j])
 
-        return dp[len(text1)][len(text2)]
-
+        return dp[m-1][n-1]
