6a20d33564672476256001b0774a89921c2c8d1b
Tathagat Sathe <66299313+tathagatsathe@users.noreply.github.com>
2025-12-20 22:50:23 +0530

    Time: 99 ms (7.33%) | Memory: 18.4 MB (37.07%) - LeetSync

---
+++ 981-delete-columns-to-make-sorted/delete-columns-to-make-sorted.py
@@ -0,0 +1,10 @@
+class Solution:
+    def minDeletionSize(self, strs: List[str]) -> int:
+        ans = 0
+        for i in range(len(strs[0])):
+            for j in range(1, len(strs)):
+                if ord(strs[j][i])<ord(strs[j-1][i]):
+                    ans+=1
+                    break
+
+        return ans
