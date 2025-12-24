a6946f939ec088b2140314c31285bdd40b993073
Tathagat Sathe <66299313+tathagatsathe@users.noreply.github.com>
2025-12-24 19:28:14 +0530

    Time: 465 ms (77.24%) | Memory: 52.5 MB (95.86%) - LeetSync

--- 1953-finding-mk-average/finding-mk-average.py
+++ 1953-finding-mk-average/finding-mk-average.py
@@ -11,7 +11,6 @@ class MKAverage:
         self.m = m
 
     def addElement(self, num: int) -> None:
-        # print(self.sum_, num, self.sl, self.dq)
         self.dq.append(num)
         flag = False
         if len(self.dq) < self.m:
@@ -19,30 +18,24 @@ class MKAverage:
         if len(self.dq) == self.m:
             self.sl = SortedList(list(self.dq))
             self.sum_ = sum(self.sl[self.k:self.m-self.k])
-            # print('self.sum_: ',self.sum_)
             return
         if len(self.dq) > self.m:
             p = self.dq.popleft()
             idx = self.sl.bisect_left(p)
-            # print('p: ',p, 'idx: ',idx, 'self.k: ',self.k, ' self.m - self.k: ',self.m - self.k)
             if self.k<=idx<self.m - self.k:
                 self.sum_-=p
                 self.sum_+=self.sl[self.m - self.k]
                 flag = True
-                # print('self.sum____ : ',self.sum_)
             elif idx<self.k:
                 self.sum_+=self.sl[self.m-self.k] - self.sl[self.k]
             self.sl.remove(p)
         self.sl.add(num)
         idx = self.sl.bisect_left(num)
-        # print('idx: ',idx, ' self.sl: ', self.sl, ' self.sum_: ', self.sum_)
         if self.k<=idx<self.m - self.k:
             self.sum_+=num - self.sl[self.m - self.k]
         elif idx<self.k:
             self.sum_+=self.sl[self.k] - self.sl[self.m - self.k]
 
-        # print(self.sum_, num, self.sl, self.dq)
-        # print('_____________________________')
 
     def calculateMKAverage(self) -> int:
         if len(self.dq) < self.m:
