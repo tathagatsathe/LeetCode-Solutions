cf7fab11c583f24bc4e6781ace3aa49af1f15e7e
Tathagat Sathe <66299313+tathagatsathe@users.noreply.github.com>
2026-01-18 20:03:33 +0530

    Time: 333 ms (52.30%) | Memory: 24 MB (6.32%) - LeetSync

--- 1311-largest-magic-square/largest-magic-square.py
+++ 1311-largest-magic-square/largest-magic-square.py
@@ -1,8 +1,8 @@
 class Solution:
     def largestMagicSquare(self, grid: List[List[int]]) -> int:
         m, n = len(grid), len(grid[0])
-        # m, n = 50, 50
         topLeft = []
+
         for j in range(n, 1, -1):
             dp = [[0]*(n-j+1) for _ in range(m)]
             for i in range(m):
@@ -10,12 +10,8 @@ class Solution:
                 dp[i][0] = temp_sum
                 for k in range(1,n-j+1):
                     temp_sum = temp_sum - grid[i][k-1] + grid[i][k+j-1]
-
                     dp[i][k] = temp_sum
                     
-
-            
-            # print(dp)
             for k in range(m-j+1):
                 count = 1
                 temp_i = 0
@@ -28,10 +24,6 @@ class Solution:
                     temp_i = i
                     if count == j:
                         topLeft.append([k, i, j, sum_])
-                        # print('k: ',k, 'i: ',i)
-                        # break
-
-        # print(topLeft)
 
         for x, y, j, sum_ in topLeft:
             c = 0
@@ -47,64 +39,13 @@ class Solution:
                     checkDiag = True
 
             if checkDiag:
-                # print(j, x, temp_i)
                 sum1 = 0
                 sum2 = 0
                 for i in range(j):
-                    # print('index1: ', x+i, temp_i+i, grid[x+i][y+i])
-                    # print('index2: ',x+j-i-1, y+i, grid[x+j-i-1][y+i])
                     sum1+=grid[x+i][y + i]
                     sum2+=grid[x+j-i-1][y+i]
-                # print('sum1, sum2, sum_: ', sum1, sum2, sum_)
                 if sum1 == sum_ and sum2 == sum_:
                     return j
-
-                # if count == j:
-                #     c = 0
-                #     checkDiag = False
-                #     topleft = [temp_i, k]
-                #     for i in range(temp_i, temp_i+j):
-                #         temp = 0
-                #         for q in range(k, k+j):
-                #             temp+=grid[q][i]
-
-                #         if temp == sum_:
-                #             c+=1
-                #         if c == j:
-                #             checkDiag = True
-
-                #     if checkDiag:
-                #         print(j, k, temp_i)
-                #         sum1 = 0
-                #         sum2 = 0
-                #         for i in range(j):
-                #             print('index1: ', k+i, temp_i+i, grid[k+i][temp_i + i])
-                #             print('index2: ',k+j-i-1, temp_i+i, grid[k+j-i-1][temp_i+i])
-                #             sum1+=grid[k+i][temp_i + i]
-                #             sum2+=grid[k+j-i-1][temp_i+i]
-                #         print('sum1, sum2, sum_: ', sum1, sum2, sum_)
-                #         if sum1 == sum_ and sum2 == sum_:
-                #             return j
-                        
-                
-
-
-
-        # print('________________________________________')
-
-        # for j in range(m, 1, -1):
-        #     dp = [[0]*n for _ in range(m-j+1)]
-        #     for i in range(n):
-        #         temp_sum = 0
-        #         for l in range(j):
-        #             temp_sum+=grid[l][i]
-
-        #         dp[0][i] = temp_sum
-        #         for k in range(1, m-j+1):
-        #             temp_sum = temp_sum - grid[k-1][i] + grid[k+j-1][i]
-        #             dp[k][i] = temp_sum
-            
-        #     print(dp)
 
         return 1
 
