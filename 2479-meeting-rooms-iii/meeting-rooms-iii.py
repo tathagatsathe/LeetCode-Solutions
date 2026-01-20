a54b2135f5c158e7bf4baebc8bd7a77a1fa4fb5c
Tathagat Sathe <66299313+tathagatsathe@users.noreply.github.com>
2026-01-21 01:12:29 +0530

    Time: 429 ms (26.87%) | Memory: 62.1 MB (5.09%) - LeetSync

--- 2479-meeting-rooms-iii/meeting-rooms-iii.py
+++ 2479-meeting-rooms-iii/meeting-rooms-iii.py
@@ -4,13 +4,12 @@ class Solution:
     def mostBooked(self, n: int, meetings: List[List[int]]) -> int:
         h1 = []
         h2 = []
-        count = [0]*n
-        for start, end in meetings:
-            heapq.heappush(h1, (start, -1, end))
-
-        room = 0
         empty_rooms = list(range(n))
         heapq.heapify(empty_rooms)
+        count = [0]*n
+        
+        for start, end in meetings:
+            heapq.heappush(h1, (start, -1, end))
 
         while h1:
             if len(h2) < n:
@@ -22,8 +21,6 @@ class Solution:
                 heapq.heappush(h2, (end, room))
                 count[room]+=1
             else:
-                # print('h1: ', h1)
-                # print('h2: ', h2)
                 st, c, en = heapq.heappop(h1)
                 while h2 and h2[0][0]<=st:
                     end, room = heapq.heappop(h2)
@@ -37,25 +34,4 @@ class Solution:
                     heapq.heappush(h2, (en + delay, room))
                 count[room]+=1
 
-        # print('count: ',count)
-
         return count.index(max(count))
-
-# h1:  [(29, -1, 49), (41, -1, 43), (47, -1, 49)]
-# h2:  [(15, 1), (27, 0), (36, 2)]
-# count = [1, 1, 1]
-
-
-# h1: [(41, -1, 43), (47, -1, 49)]
-# h2: [(36, 2),(49, 0)]
-# empty_rooms: [1]
-# count = [2, 1, 1]
-
-
-# h1: [(47, -1, 49)]
-# h2: [(36, 2),(43, 1), (49, 0)]
-# empty_rooms: []
-
-
-# [[1,27],[11,15],[15,36],[29,49],[41,43],[47,49]]
-
