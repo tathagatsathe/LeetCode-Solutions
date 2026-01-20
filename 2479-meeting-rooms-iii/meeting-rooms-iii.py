a18618d77b82e8027fe6d56a3f4079fc4f2775b5
Tathagat Sathe <66299313+tathagatsathe@users.noreply.github.com>
2026-01-21 01:20:37 +0530

    Time: 217 ms (37.66%) | Memory: 57.4 MB (5.09%) - LeetSync

--- 2479-meeting-rooms-iii/meeting-rooms-iii.py
+++ 2479-meeting-rooms-iii/meeting-rooms-iii.py
@@ -2,36 +2,25 @@ import heapq
 
 class Solution:
     def mostBooked(self, n: int, meetings: List[List[int]]) -> int:
-        h1 = []
-        h2 = []
+        h = []
+        meetings.sort()
         empty_rooms = list(range(n))
         heapq.heapify(empty_rooms)
         count = [0]*n
         
         for start, end in meetings:
-            heapq.heappush(h1, (start, -1, end))
+            while h and h[0][0]<=start:
+                _, room = heapq.heappop(h)
+                heapq.heappush(empty_rooms, room)
 
-        while h1:
-            if len(h2) < n:
-                start, c, end = heapq.heappop(h1)
-                while h2 and h2[0][0]<=start:
-                    en, room = heapq.heappop(h2)
-                    heapq.heappush(empty_rooms, room)
+            if empty_rooms:
                 room = heapq.heappop(empty_rooms)
-                heapq.heappush(h2, (end, room))
+                heapq.heappush(h, (end, room))
                 count[room]+=1
             else:
-                st, c, en = heapq.heappop(h1)
-                while h2 and h2[0][0]<=st:
-                    end, room = heapq.heappop(h2)
-                    heapq.heappush(empty_rooms, room)
-                if empty_rooms:
-                    room = heapq.heappop(empty_rooms)
-                    heapq.heappush(h2, (en, room))
-                else:
-                    end2, room = heapq.heappop(h2)
-                    delay = max(end2 - st, 0)
-                    heapq.heappush(h2, (en + delay, room))
+                en, room = heapq.heappop(h)
+                delay = en - start
+                heapq.heappush(h, (end+delay, room))
                 count[room]+=1
 
         return count.index(max(count))
