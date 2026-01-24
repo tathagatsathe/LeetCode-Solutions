a63d3dbdeb022729b74c15c750d48cad4a33f32a
Tathagat Sathe <66299313+tathagatsathe@users.noreply.github.com>
2026-01-24 23:28:20 +0530

    Time: 2897 ms (45.02%) | Memory: 72 MB (25.54%) - LeetSync

--- 3772-minimum-pair-removal-to-sort-array-ii/minimum-pair-removal-to-sort-array-ii.py
+++ 3772-minimum-pair-removal-to-sort-array-ii/minimum-pair-removal-to-sort-array-ii.py
@@ -18,42 +18,30 @@ class Solution:
             else:
                 adjacents[i] = [i-1, i+1]
         
-        # print('inversions: ',inversions)
         deleted = [False]*n
         ans = 0
         while inversions and h:
-            # print('______________________________________________________')
             sum_, idx = heapq.heappop(h)
             prev, nxt = adjacents[idx]
-            # print('nums: ',nums)
-            # print('adjacents: ',adjacents)
-            # print('pair_sums: ',pair_sums)
             if sum_ != pair_sums[idx] or deleted[idx] == True:
                 continue
             if nxt < n:
                 if nums[idx] > nums[nxt]:
-                    # print('nxt')
                     inversions-=1
                 next_ = adjacents[nxt][1]
                 if next_ < n:
                     if nums[nxt] <= nums[next_] and sum_ > nums[next_]:
-                        # print('next_')
                         inversions+=1
                     if nums[nxt]>nums[next_] and sum_<=nums[next_]:
                         inversions-=1
             
             if prev >=0:
                 if nums[prev]>nums[idx] and sum_>=nums[prev]:
-                    # print('prev')
                     inversions-=1
                 if nums[idx]>=nums[prev] and sum_<nums[prev]:
                     inversions+=1
-
-            
-            
-            
+   
             nums[idx] = sum_
-            # print('idx: ',idx, prev, nxt, ' inversions: ',inversions, ' sum_: ',sum_, ' ans: ', ans)
 
             if nxt<n:
                 ans+=1
