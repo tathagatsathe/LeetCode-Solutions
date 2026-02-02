5cf945c2cafe4bc99f56980f1e25bb36ac051c60
Tathagat Sathe <66299313+tathagatsathe@users.noreply.github.com>
2026-02-03 00:22:21 +0530

    Time: 3 ms (64.65%) | Memory: 19.4 MB (15.67%) - LeetSync

--- 43-multiply-strings/multiply-strings.py
+++ 43-multiply-strings/multiply-strings.py
@@ -1,4 +1,14 @@
 class Solution:
     def multiply(self, num1: str, num2: str) -> str:
         
-        return str(int(num1)*int(num2))
+        def convertToInt(num):
+            temp = 0
+            n = len(num)
+            order = 0
+            for i in range(n-1, -1, -1):
+                temp+= (ord(num[i]) - ord("0"))*10**order
+                order+=1
+
+            return temp
+
+        return str(convertToInt(num1) * convertToInt(num2))
