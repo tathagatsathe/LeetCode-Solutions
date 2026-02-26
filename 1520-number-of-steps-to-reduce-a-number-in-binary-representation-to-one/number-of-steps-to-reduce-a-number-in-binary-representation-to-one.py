f723a4bb300b33a6abe58c6c3124817e9ec7d234
Tathagat Sathe <66299313+tathagatsathe@users.noreply.github.com>
2026-02-26 22:27:16 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (80.78%) - LeetSync

--- 1520-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/number-of-steps-to-reduce-a-number-in-binary-representation-to-one.py
+++ 1520-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/number-of-steps-to-reduce-a-number-in-binary-representation-to-one.py
@@ -1,24 +1,13 @@
 class Solution:
     def numSteps(self, s: str) -> int:
         ans = 0
-
-        def binaryToDecimal(s):
-            res = 0
-            n = len(s)
-            for i in range(n):
-                res+=int(s[i])*2**(n-i-1)
-
-            return res
-
-        n = binaryToDecimal(s)
-
+        n = int(s,2)
 
         while n!=1:
             if n%2:
                 n+=1
             else:
                 n//=2
-
             ans+=1
 
         return ans
