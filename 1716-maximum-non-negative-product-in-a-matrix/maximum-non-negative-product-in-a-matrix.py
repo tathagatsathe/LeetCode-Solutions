3d21e3a65e0367a67011e25a01b035bc6a4c5dd9
Tathagat Sathe <66299313+tathagatsathe@users.noreply.github.com>
2026-03-23 17:44:31 +0530

    Time: 1 ms (92.11%) | Memory: 19.6 MB (48.25%) - LeetSync

--- 1716-maximum-non-negative-product-in-a-matrix/maximum-non-negative-product-in-a-matrix.py
+++ 1716-maximum-non-negative-product-in-a-matrix/maximum-non-negative-product-in-a-matrix.py
@@ -1,128 +1,22 @@
 class Solution:
     def maxProductPath(self, grid: List[List[int]]) -> int:
         m, n = len(grid), len(grid[0])
-        mod = 10**9 + 7
         dp = [[[0,0]]*n for _ in range(m)]
-        zero = False
+
         for i in range(m):
             for j in range(n):
-                if grid[i][j] == 0:
-                    zero = True
-                min_, max_ = 0, 0
-                temp = None
-                if i==0 and j == 0:
-                    if grid[i][j] > 0:
-                        max_ = grid[i][j]
-                    else:
-                        min_ = abs(grid[i][j])
-                elif i == 0 and j > 0:
-                    if grid[i][j] < 0:
-                        max_ = dp[i][j-1][0]*abs(grid[i][j])
-                        min_ = dp[i][j-1][1]*abs(grid[i][j])
-                    else:
-                        min_ = dp[i][j-1][0]*grid[i][j]
-                        max_ = dp[i][j-1][1]*grid[i][j]
-                elif j == 0 and i > 0:
-                    if grid[i][j] < 0:
-                        max_ = dp[i-1][j][0]*abs(grid[i][j])
-                        min_ = dp[i-1][j][1]*abs(grid[i][j])
-                    else:
-                        min_ = dp[i-1][j][0]*grid[i][j]
-                        max_ = dp[i-1][j][1]*grid[i][j]
-                elif i > 0 and j > 0:
-                    if grid[i][j] < 0:
-                        max_ = max(dp[i-1][j][0], dp[i][j-1][0]) * abs(grid[i][j])
-                        min_ = max(dp[i-1][j][1], dp[i][j-1][1]) * abs(grid[i][j])
-                    else:
-                        max_ = max(dp[i-1][j][1], dp[i][j-1][1]) * abs(grid[i][j])
-                        min_ = max(dp[i-1][j][0], dp[i][j-1][0]) * abs(grid[i][j])
+                temp = []
+                if i > 0:
+                    temp.extend([grid[i][j] * dp[i-1][j][0], grid[i][j] * dp[i-1][j][1]])
+                if j > 0:
+                    temp.extend([grid[i][j] * dp[i][j-1][0], grid[i][j] * dp[i][j-1][1]])
+                if i == 0 and j == 0:
+                    temp.append(grid[i][j])
 
-                dp[i][j] = [min_, max_]
-            
+                dp[i][j] = [min(temp), max(temp)]
 
+        res = dp[m-1][n-1][1]
 
-        if zero == False and dp[m-1][n-1][1] == 0:
-            return -1
-
-        return dp[m-1][n-1][1] % mod
-
-            
-# class Solution:
-#     def maxProductPath(self, grid: List[List[int]]) -> int:
-#         m, n = len(grid), len(grid[0])
-#         mod = 10**9 + 7
-#         dp = [[[0,0]]*n for _ in range(m)]
-#         zero = False
-#         for i in range(m):
-#             for j in range(n):
-#                 if grid[i][j] == 0:
-#                     zero = True
-#                 min_, max_ = None, None
-
-#                 if i==0 and j == 0:
-#                     if grid[i][j] > 0:
-#                         max_ = grid[i][j]
-#                     else:
-#                         min_ = abs(grid[i][j])
-#                 elif i == 0 and j > 0:
-#                     if grid[i][j] < 0:
-#                         if dp[i][j-1][0] != None:
-#                             max_ = dp[i][j-1][0]*abs(grid[i][j]) % mod
-#                         if dp[i][j-1][1] != None:
-#                             min_ = dp[i][j-1][1]*abs(grid[i][j]) % mod
-#                     else:
-#                         if dp[i][j-1][0] != None:
-#                             min_ = dp[i][j-1][0]*grid[i][j] % mod
-#                         if dp[i][j-1][1] != None:
-#                             max_ = dp[i][j-1][1]*grid[i][j] % mod
-#                 elif j == 0 and i > 0:
-#                     if grid[i][j] < 0:
-#                         if dp[i-1][j][0] != None:
-#                             max_ = dp[i-1][j][0]*abs(grid[i][j]) % mod
-#                         if dp[i-1][j][1] != None:
-#                             min_ = dp[i-1][j][1]*abs(grid[i][j]) % mod
-#                     else:
-#                         if dp[i-1][j][0] != None:
-#                             min_ = dp[i-1][j][0]*grid[i][j] % mod
-#                         if dp[i-1][j][1] != None:
-#                             max_ = dp[i-1][j][1]*grid[i][j] % mod
-#                 elif i > 0 and j > 0:
-#                     if grid[i][j] < 0:
-#                         if dp[i-1][j][0] != None and dp[i][j-1][0] != None:
-#                             max_ = max(dp[i-1][j][0], dp[i][j-1][0]) * abs(grid[i][j]) % mod
-#                         elif dp[i-1][j][0] != None:
-#                             max_ = dp[i-1][j][0] * abs(grid[i][j]) % mod
-#                         elif dp[i][j-1][0] != None:
-#                             max_ = dp[i][j-1][0] * abs(grid[i][j]) % mod
-
-#                         if dp[i-1][j][1] !=None and dp[i][j-1][1] != None:
-#                             min_ = max(dp[i-1][j][1], dp[i][j-1][1]) * abs(grid[i][j]) % mod
-#                         elif dp[i][j-1][1] != None:
-#                             min_ = dp[i][j-1][1] * abs(grid[i][j]) % mod
-#                         elif dp[i-1][j][1] != None:
-#                             min_ = dp[i-1][j][1] * abs(grid[i][j]) % mod
-#                     else:
-#                         if dp[i-1][j][1] !=None and dp[i][j-1][1] != None:
-#                             max_ = max(dp[i-1][j][1], dp[i][j-1][1]) * abs(grid[i][j]) % mod
-#                         elif dp[i][j-1][1] != None:
-#                             max_ = dp[i][j-1][1] * abs(grid[i][j]) % mod
-#                         elif dp[i-1][j][1] != None:
-#                             max_ = dp[i-1][j][1] * abs(grid[i][j]) % mod
-
-#                         if dp[i-1][j][0] !=None and dp[i][j-1][0] != None:
-#                             min_ = max(dp[i-1][j][0], dp[i][j-1][0]) * abs(grid[i][j]) % mod
-#                         elif dp[i][j-1][0] != None:
-#                             min_ = dp[i][j-1][0] * abs(grid[i][j]) % mod
-#                         elif dp[i-1][j][0] != None:
-#                             min_ = dp[i-1][j][0] * abs(grid[i][j]) % mod
-
-#                 dp[i][j] = [min_, max_]
-            
-#         print(dp)
-
-#         if dp[m-1][n-1][1] == None:
-#             return -1
-
-#         return dp[m-1][n-1][1] % mod
+        return res % (10**9 + 7) if res >= 0 else -1
 
             
