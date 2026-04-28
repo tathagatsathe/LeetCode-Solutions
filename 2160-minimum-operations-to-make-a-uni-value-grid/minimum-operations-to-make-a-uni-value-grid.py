64dc4fb4c10736462aebf147e208fa177e0a43ca
Tathagat Sathe <66299313+tathagatsathe@users.noreply.github.com>
2026-04-28 19:34:41 +0530

    Time: 148 ms (69.18%) | Memory: 40.8 MB (93.84%) - LeetSync

--- 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.py
+++ 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.py
@@ -1,17 +1,12 @@
 class Solution:
     def minOperations(self, grid: List[List[int]], x: int) -> int:
         flat_list = [item for sublist in grid for item in sublist]
-
         flat_list.sort()
-        
-        # print(flat_list)
         n = len(flat_list)
 
         mid = n//2
-        # print(n)
-        # print(mid)
-        # print(flat_list[mid])
         ans = 0
+        
         for i in range(mid):
             diff = (flat_list[mid] - flat_list[i])
             if diff % x != 0:
@@ -26,6 +21,3 @@ class Solution:
 
         return ans
 
-
-# [57, 69, 105, 105, 141, 177, 273, 309, 345, 417, 429, 453, 465, 
-# 609, 657, 657, 681, 681, 717, 801, 885, 897, 897, 921, 921, 933, 945]
