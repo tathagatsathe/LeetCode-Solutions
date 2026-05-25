d224048417b8aceb00d640eb56afe0439fcd47a8
Tathagat Sathe <66299313+tathagatsathe@users.noreply.github.com>
2026-05-26 00:30:11 +0530

    Time: 230 ms (16.58%) | Memory: 24.1 MB (19.37%) - LeetSync

--- 2001-jump-game-vii/jump-game-vii.py
+++ 2001-jump-game-vii/jump-game-vii.py
@@ -3,11 +3,10 @@ from collections import deque
 class Solution:
     def canReach(self, s: str, minJump: int, maxJump: int) -> bool:
         n = len(s)
-        print('n: ',n)
         visited = [False]*n
         dq = deque([0])
         visited[0] = True
-        max_ = 0
+        last_index = 0
 
         while dq:
             idx = dq.popleft()
@@ -15,13 +14,11 @@ class Solution:
             if idx == n-1:
                 return True
 
-            # dq.append(idx+1)
-
-            for j in range(max(idx+minJump, max_), min(idx+maxJump+1,n)):
+            for j in range(max(idx+minJump, last_index), min(idx+maxJump+1,n)):
                 if s[j] == "0" and visited[j] == False:
                     dq.append(j)
                     visited[j] = True
 
-            max_ = min(idx+maxJump+1,n)
+            last_index = min(idx+maxJump+1,n)
 
         return False
