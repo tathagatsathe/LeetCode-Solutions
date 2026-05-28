f638306a8009dab81c1e942890c6078728ccf088
Tathagat Sathe <66299313+tathagatsathe@users.noreply.github.com>
2026-05-29 01:59:46 +0530

    Time: 1699 ms (25.55%) | Memory: 204.5 MB (17.22%) - LeetSync

---
+++ 3376-longest-common-suffix-queries/longest-common-suffix-queries.py
@@ -0,0 +1,65 @@
+class TrieNode:
+    def __init__(self):
+        self.children = [None]*26
+        self.isLeaf = False
+        self.index = None
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, key, index):
+        curr = self.root
+        for c in key:
+            idx = ord(c) - ord('a')
+            if curr.children[idx] is None:
+                curr.children[idx] = TrieNode()
+            # curr.index = index
+            # print('key: ',index)
+            curr = curr.children[idx]
+            curr.index = index
+        curr.isLeaf = True
+
+    def searchPrefix(self, key):
+        curr = self.root
+        # print('curr: ',curr.index, ' key: ', key)
+        # print(curr.children[0].index if curr!=None and curr.children[0]!=None else None)
+        # print(curr.children[1].index if curr!=None and curr.children[1]!=None else None)
+        prev = curr
+        for c in key:
+            idx = ord(c) - ord('a')
+            # print('idx: ', curr.index, curr.children[idx], curr.isLeaf)
+            if curr.children[idx] is None:
+                return curr.index
+            prev = curr
+            curr = curr.children[idx]
+
+        return curr.index
+
+
+class Solution:
+    def stringIndices(self, wordsContainer: List[str], wordsQuery: List[str]) -> List[int]:
+        ans = []
+
+        trie = Trie()
+        
+        wordsContainer = [(len(word), idx, word[::-1]) for idx, word in enumerate(wordsContainer)]
+        wordsContainer.sort(reverse=True)
+
+        # print(wordsContainer)
+
+        min_idx = wordsContainer[-1][1]
+
+        for _, idx, word in wordsContainer:
+            trie.insert(word, idx)
+
+        for query in wordsQuery:
+            l = trie.searchPrefix(query[::-1])
+            if l == None:
+                ans.append(min_idx)
+                continue
+            ans.append(l)
+
+        return ans
+
+
