a9133b165b5427471b45a15a7f2b9188cf35d92f
Tathagat Sathe <66299313+tathagatsathe@users.noreply.github.com>
2026-06-27 17:44:54 +0530

    Time: 256 ms (23.62%) | Memory: 32.4 MB (14.17%) - LeetSync

--- 3299-find-the-maximum-number-of-elements-in-subset/find-the-maximum-number-of-elements-in-subset.py
+++ 3299-find-the-maximum-number-of-elements-in-subset/find-the-maximum-number-of-elements-in-subset.py
@@ -5,33 +5,24 @@ class Solution:
         ans = 1
         count_one = nums.count(1)
         if count_one:
-            if count_one % 2:
-                ans = count_one
-            else:
-                ans = count_one - 1
+            ans = count_one if count_one % 2 else count_one - 1
 
-        # print(nums)
         c = Counter(nums)
         sorted_c = sorted(list(set(nums)))
-        # print(sorted(c.items()))
 
         def isPerfectSquare(n):
-            return n**0.5 - int(n**0.5) == 0
-        
-        map_ = {1:0}
+            return n**0.5 == int(n**0.5)
+
+        map_ = {}
 
         for val in sorted_c:
             if val == 1:
                 continue
-            if c[val] >= 1 and val not in map_:
-                sqrt = int(val**0.5)
-                if isPerfectSquare(val) and sqrt in c and c[sqrt] > 1:
-                    map_[sqrt]+=1
-                    map_[val] = map_[sqrt] + 1
-                else:
-                    map_[val] = 1
 
-        # print(max(map_.values()))
-        ans = max(max(map_.values()),ans)
-        # print(map_)
+            map_[val] = 1
+            sqrt = int(val**0.5)
+            if isPerfectSquare(val) and sqrt in c and c[sqrt] > 1:
+                map_[val] = map_[sqrt] + 2
+            ans = max(ans, map_[val])
+            
         return ans
