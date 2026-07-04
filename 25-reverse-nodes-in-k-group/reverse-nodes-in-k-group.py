93b37736639188456b179272d76248d9ba2fba6e
Tathagat Sathe <66299313+tathagatsathe@users.noreply.github.com>
2026-07-05 03:39:30 +0530

    Time: 3 ms (35.03%) | Memory: 20.3 MB (46.19%) - LeetSync

--- 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.py
+++ 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.py
@@ -14,67 +14,27 @@ class Solution:
         head = root
         pre = root
         root = root.next
-        # print(root)
+
+
         while root:
             node = root
-            
-            for i in range(k-1):
-                if node!=None:
-                    node = node.next
 
-            if node == None:
-                break
+            for i in range(k):
+                if node == None:
+                    return head.next
+                node = node.next
 
-            p = root
-            r = root
-            # print('p: ', p)
+            first_node = root
+            kth_node = root
             
             for i in range(k - 1):
-                temp = r.next
-                r.next = r.next.next
-                temp.next = p
-                p = temp
+                temp = kth_node.next
+                kth_node.next = kth_node.next.next
+                temp.next = first_node
+                first_node = temp
 
-            temp1 = pre
-            temp1.next = p
-            pre = r
-            # print(pre)
-            # print('____________________')
-            # print(root)
+            pre.next = first_node
+            pre = kth_node
             root = root.next
                     
         return head.next
-
-        
-
-
-
-#   r
-#   p   nd
-# # 1 -> 2 -> 3 -> 4 -> 5 -> 6 -> 7 -> 8 -> 9
-
-# temp = r.next.next
-# r.next.next = p
-# p = r.next
-# r.next = temp
-
-# temp = 3
-# r.next.next = 1
-# r.next = 3
-
-# # 1 -> 3 -> 4 -> 5 -> 6 -> 7 -> 8 -> 9
-
-#   p    r   
-# # 2 -> 1 -> 3 -> 4 -> 5 -> 6 -> 7 -> 8 -> 9
-
-# temp = 4
-# r.next.next = 2   3 -> 2
-# p = r.next
-# r.next = 4
-
-#   p         r   
-# # 3 -> 2 -> 1 -> 4 -> 5 -> 6 -> 7 -> 8 -> 9
-
-# # 1 -> 2 -> 4 -> 3 -> 5 -> 6 -> 7 -> 8 -> 9
-
-
