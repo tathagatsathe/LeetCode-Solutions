1937a76b0583c848d06d638493b62135c138dc33
Tathagat Sathe <66299313+tathagatsathe@users.noreply.github.com>
2026-07-06 01:27:09 +0530

    Time: 299 ms (5.40%) | Memory: 20.4 MB (21.09%) - LeetSync

---
+++ 572-subtree-of-another-tree/subtree-of-another-tree.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+
+        def serialize(root):
+            """Converts a binary tree into a string expression."""
+            if not root:
+                return "#"
+            return f"{root.val},{serialize(root.left)},{serialize(root.right)}"
+
+        if serialize(root) == serialize(subRoot):
+            return True
+        if root == None:
+            return False
+
+        return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
