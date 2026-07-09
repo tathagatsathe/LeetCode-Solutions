34f2d815735ac20079b0864b1d81eaefc3e7a714
Tathagat Sathe <66299313+tathagatsathe@users.noreply.github.com>
2026-07-09 23:05:58 +0530

    Time: 92 ms (61.11%) | Memory: 50.4 MB (32.54%) - LeetSync

--- 3838-path-existence-queries-in-a-graph-i/path-existence-queries-in-a-graph-i.py
+++ 3838-path-existence-queries-in-a-graph-i/path-existence-queries-in-a-graph-i.py
@@ -2,24 +2,13 @@ class Solution:
     def pathExistenceQueries(self, n: int, nums: List[int], maxDiff: int, queries: List[List[int]]) -> List[bool]:
         ans = []
         n = len(nums)
-        parent = list(range(n))
-        
-        def findParent(u):
-            if u != parent[u]:
-                return findParent(parent[u])
-            return parent[u]
-
-        def union(u, v):
-            parent_u = findParent(u)
-            parent_v = findParent(v)
-
-            if parent_u != parent_v:
-                parent[u] = parent_v
-
+        parent = [0]*n
 
         for i in range(1, n):
             if abs(nums[i] - nums[i-1]) <= maxDiff:
-                union(i, i-1)
+                parent[i] = parent[i-1]
+            else:
+                parent[i] = parent[i-1] + 1
 
         for u, v in queries:
             ans.append(parent[u] == parent[v])
