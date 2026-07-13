3efb81e73bdc37df8ce823a66d5d5ebd8c8e8a58
Tathagat Sathe <66299313+tathagatsathe@users.noreply.github.com>
2026-07-13 22:38:04 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (88.24%) - LeetSync

--- 1212-sequential-digits/sequential-digits.py
+++ 1212-sequential-digits/sequential-digits.py
@@ -12,10 +12,6 @@ class Solution:
             if num%10 != 9:
                 fn(num*10 + (num%10+1))
 
-        no_of_digits = len(str(low))
-
-        min_, max_ = min(int(str(low)[0]),  int(str(high)[0])), max(int(str(low)[0]),  int(str(high)[0]))
-
         for i in range(1, 10):
             fn(i)
 
