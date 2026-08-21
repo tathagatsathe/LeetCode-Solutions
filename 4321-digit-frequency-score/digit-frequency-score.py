8a5b558e045fbbac2c116eeea4a71dbee2cd92d8
Tathagat Sathe <66299313+tathagatsathe@users.noreply.github.com>
2026-08-21 16:09:15 +0530

    Time: 3 ms (25.58%) | Memory: 19.4 MB (21.12%) - LeetSync

--- 4321-digit-frequency-score/digit-frequency-score.py
+++ 4321-digit-frequency-score/digit-frequency-score.py
@@ -1,17 +1,9 @@
 class Solution:
     def digitFrequencyScore(self, n: int) -> int:
-        freq = {}
-
+        ans = 0
         while n:
             rem = n%10
-            if rem not in freq:
-                freq[rem] = 0
-            freq[rem]+=1
+            ans+=rem
             n//=10
         
-        ans = 0
-
-        for d, val in freq.items():
-            ans+=(d*val)
-
         return ans
