2c55060ad731ce84436253f815f21f737c63fe40
Tathagat Sathe <66299313+tathagatsathe@users.noreply.github.com>
2026-08-31 01:13:39 +0530

    Time: 54 ms (8.19%) | Memory: 32.5 MB (5.40%) - LeetSync

--- 134-gas-station/gas-station.py
+++ 134-gas-station/gas-station.py
@@ -1,18 +1,30 @@
 class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
-        if(sum(gas)<sum(cost)):
-            return -1
         n = len(gas)
-        sub = 0
-        ans = 0
+        temp = [x - y for x, y in zip(gas, cost)]
+        # print(temp)
+
+        prefix = []
+        suffix = []
+
+        pre = suff = 0
+        for val in temp:
+            pre+=val
+            prefix.append(pre)
+
+        for val in temp[::-1]:
+            pre+=val
+            suffix.append(pre)
+
+        suffix = suffix[::-1]
+
+        # print(prefix)
+        # print(suffix)
+        max_idx = 0
+
         for i in range(n):
-            val = gas[i]-cost[i]
-            sub+=val
-            if(sub<0):
-                ans=i+1
-                sub = 0
+            if suffix[i] > suffix[max_idx]:
+                max_idx = i
 
-        return ans
-
-        
+        return max_idx if suffix[max_idx] + prefix[max_idx-1] >= 0 else -1
         
