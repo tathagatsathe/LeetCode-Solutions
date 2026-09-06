44ce9a1ef1e5226380b7f9b2f5983771c6199094
Tathagat Sathe <66299313+tathagatsathe@users.noreply.github.com>
2026-09-06 19:59:39 +0530

    Time: 279 ms (69.34%) | Memory: 75.6 MB (56.33%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.py
+++ 115-distinct-subsequences/distinct-subsequences.py
@@ -3,29 +3,24 @@ class Solution:
         ans = 0
         n = len(s)
         m = len(t)
-        dp = [[None]*(m+1) for _ in range(n+1)]
+        dp = [[0]*(m) for _ in range(n)]
 
-        def fn(i, j):
-            nonlocal ans
 
-            if j >= m:
-                return 1
-            
-            if i >= n:
-                return 0
+        dp[0][0] = 1 if s[0] == t[0] else 0
 
-            if dp[i][j] != None:
-                return dp[i][j]
+        for i in range(1, n):
+            dp[i][0] = dp[i-1][0]
+            if s[i] == t[0]:
+                dp[i][0]+=1
 
-            a = 0
-            if s[i] == t[j]:
-                a = fn(i+1, j+1)
-            b = fn(i+1, j)
+        for i in range(1, n):
+            for j in range(1, m):
+                if j > i:
+                    continue
+                if s[i] == t[j]:
+                    dp[i][j] = dp[i-1][j] + dp[i-1][j-1]
+                else:
+                    dp[i][j] = dp[i-1][j]
 
-            dp[i][j] = a + b
-
-            return a + b
-
-        ans = fn(0, 0)
-
-        return ans
+        
+        return dp[-1][-1]
